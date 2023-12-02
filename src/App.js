09d0ae16f627fa16adfcf340f3cb87e76100bf80
OLIVER <d4rkl19ht79@gmail.com>
2023-12-02 09:10:26 -0500

    Completed Shopping App

--- src/App.js
+++ src/App.js
@@ -6,10 +6,29 @@ import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+      <AppProvider>
+        <div className="container mt-4">
+            <h1>Shopping App</h1>
+            <div className="row mb-3">
+                <div className="col-sm">
+                    <CartValue />
+                </div>
+                <div className="col-sm">
+                    <Location />
+                </div>
+            </div>
+            <div className="mb-3">
+                <h2>Shopping Cart</h2>
+                <ExpenseList />
+            </div>
+            <div>
+                <h2>Add Items</h2>
+                <ItemSelected />
+            </div>
+        </div>
+      </AppProvider>
   );
 }
 
